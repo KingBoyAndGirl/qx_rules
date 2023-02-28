be69d05bf9fde571af0d4b3dd941d8e929ade3bb
mo <48237066+KingBoyAndGirl@users.noreply.github.com>
2023-02-28 16:24:55 +0800

    Update qtzl_vip.js

--- qtzl_vip.js
+++ qtzl_vip.js
@@ -1,1 +1,19 @@
-test
+/*
+
+QX:
+[rewrite_local]
+#解锁会员
+^https:\/\/i\.at\.qq\.com\/pay\/memberinfo url script-response-body https://raw.githubusercontent.com/KingBoyAndGirl/qx_rules/main/qtzl_vip.js
+
+[mitm]
+hostname = wx-love-api.afunapp.com
+
+*/
+
+let obj = JSON.parse($response.body);
+obj = {
+  "is_vip": true,
+  "vip_type": 0
+ };
+
+$done({body: JSON.stringify(obj)});
