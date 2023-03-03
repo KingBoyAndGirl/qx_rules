31460b32239cdaf8810fb274faae0cb5ea3190a3
mo <48237066+KingBoyAndGirl@users.noreply.github.com>
2023-03-03 17:38:58 +0800

    Update qtzl_vip.js

--- qtzl_vip.js
+++ qtzl_vip.js
@@ -3,11 +3,11 @@
 QX:
 [rewrite_local]
 #解锁会员
-^https://wx-love-api\.afunapp\.com/(proporty/get_user_property|user/get_user_info) url script-response-body https://raw.githubusercontent.com/KingBoyAndGirl/qx_rules/main/qtzl_vip.js
+^https?:\/\/*\.afunapp\.com\/(proporty\/get_user_property|user\/get_user_info) url script-response-body https://raw.githubusercontent.com/KingBoyAndGirl/qx_rules/main/qtzl_vip.js
 
 
 [mitm]
-hostname = wx-love-api.afunapp.com
+hostname = *.afunapp.com
 
 */
 
