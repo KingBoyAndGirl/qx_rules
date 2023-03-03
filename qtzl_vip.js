097451aee18b7499a82f42a7e1951e1e6aac5b5c
mo <48237066+KingBoyAndGirl@users.noreply.github.com>
2023-03-03 18:00:26 +0800

    Update qtzl_vip.js

--- qtzl_vip.js
+++ qtzl_vip.js
@@ -11,11 +11,13 @@ hostname = *.afunapp.com
 
 */
 
-let obj = JSON.parse($response.body);
+var body = $response.body;
+var obj = JSON.parse(body);
 console.log('Hello, world!');
 console.log(obj);
 obj.data.is_vip = true;
 obj.data.vip_type = 0;
 console.log('===========');
 console.log(obj);
-$done({body: JSON.stringify(obj)});
+body = JSON.stringify(obj);
+$done(body);
